818bc8164da41c373dc9e162f06405fd1c174bda
Bryant <bryantsuiskensmast@gmail.com>
2021-12-15 14:25:04 +0100

    App done

--- WeatherApp/WeatherApp/Models/OpenWeather models/WeatherOutput.cs
+++ WeatherApp/WeatherApp/Models/OpenWeather models/WeatherOutput.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
+using System.Windows.Media;
+using System.Windows;
 
 namespace WeatherApp.Models
 {
@@ -10,14 +13,34 @@ namespace WeatherApp.Models
 
         [JsonPropertyName("current")]
         public WeatherReportHourly Current { get; set; }
-        [JsonPropertyName("minutely")]
-        public MinuteReport[] Minutely { get; set; }
         [JsonPropertyName("hourly")]
         public WeatherReportHourly[] Hourly { get; set; }
         [JsonPropertyName("daily")]
         public WeatherReportDaily[] Daily { get; set; }
 
+        public PointCollection TempPoints { get {
+                PointCollection temp = new PointCollection();
+                Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds -2;
+                foreach (WeatherReportHourly hour in Hourly)
+                {
+                    temp.Add(new Point((hour.Dt - unixTimestamp) /1200 , Math.Round(hour.Temp, 3)/2 + 30 ));
+                }
+                return temp;
+            
+            } }
+        public PointCollection RainPoints
+        {
+            get
+            {
+                PointCollection rain = new PointCollection();
+                Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds - 2;
+                foreach (WeatherReportHourly hour in Hourly)
+                {
+                    rain.Add(new Point((hour.Dt - unixTimestamp) / 1200, hour.Clouds / 4 + 1));
+                }
+                return rain;
 
-
+            }
+        }
     }
 }
